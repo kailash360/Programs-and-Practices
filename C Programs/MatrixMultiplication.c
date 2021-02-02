87ca1c610572bc21e63d024d6b70922337af0b67
kailash360 <kailashkejriwal21@gmail.com>
2021-02-02 16:30:36 +0530

    matrix multiplication

---
+++ C Programs/MatrixMultiplication.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+void getMatrixElements(int matrix[][10],int row,int column) 
+{
+
+    for(int i=0;i<row;i++) 
+    {
+        printf("Enter element for row %d:",i+1);
+        for(int j=0;j<column;j++)
+        {scanf("%d",&matrix[i][j]);}
+    }
+}
+
+void multiplyMatrices(int first[][10],int second[][10],int result[][10],int r1, int c1, int r2, int c2)
+{
+    for (int i=0;i<r1;++i)
+    {
+      for(int j=0;j<c2;j++) 
+      {result[i][j]=0;}
+    }
+    for (int i=0;i<r1;++i) 
+    {
+        for (int j=0;j<c2;++j) 
+        {
+            for (int k=0;k<c1;++k) 
+            {result[i][j]+=first[i][k]*second[k][j];}
+        }
+    }
+}
+
+void display(int result[][10],int row,int column) 
+{
+    for (int i=0;i<row;i++) 
+    { 
+        for (int j=0;j<column;j++) 
+        {
+            printf("%d ",result[i][j]);
+            if (j==column-1)
+            {printf("\n");}
+        }
+    }
+}
+
+int main() 
+{
+    int first[10][10],second[10][10],result[10][10],r1,c1,r2,c2;
+    printf("Enter rows and column for the first matrix: ");
+    scanf("%d %d",&r1,&c1);
+    printf("Enter rows and column for the second matrix: ");
+    scanf("%d %d",&r2,&c2);
+
+    while (c1!=r2) 
+    {
+        printf("Error! Enter rows and columns again.\n");
+        printf("Enter rows and columns for the first matrix: ");
+        scanf("%d%d",&r1,&c1);
+        printf("Enter rows and columns for the second matrix: ");
+        scanf("%d%d",&r2,&c2);
+    }
+   
+
+    printf("For first matrix\n");
+    getMatrixElements(first,r1,c1);
+    printf("For second matrix\n");
+    getMatrixElements(second,r2,c2);
+
+    printf("First matrix is\n");
+    display(first,r1,c1);
+    printf("Second matrix is\n");
+    display(second,r2,c2);
+ 
+    multiplyMatrices(first,second,result,r1,c1,r2,c2);
+
+    display(result,r1,c2);
+
+    return 0;
+}
